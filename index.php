5bf1e22a1268d93c4085e9e54890a8209af4f4af
rittoaries <ritto.m@ariesenergyglobal.com>
2024-12-06 17:15:07 +0530

    add intermediate line

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 echo "This Header";
 echo "This message is added by Ritto";
 echo "This body";
+echo "intermediate line";
 echo "This footer";
 echo "privacy policy";
-echo "test line";
+//echo "test line";
 ?>
